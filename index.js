0c09b8ef723987286634afef93440efb6eadfe69
gnosisk <gnosisk@naver.com>
2023-06-08 23:51:52 +0000

    test1 directory delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer( (req, res) => {
   console.log('Requested: ' + req.url);
   res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
   res.write('<h1>docker 예제1</h1>');
-  res.write('<h2>Hello 지유~!</h2>');
+  res.write('<h2>Hello 지유야 항상 건강하고 행복하자~</h2>');
   res.end();
 }).listen(port, () => {
   console.log('Listen ... ' + os.hostname()+':'+port);
